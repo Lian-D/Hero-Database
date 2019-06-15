b9d17ea9f8683b551f3421a763aad1894274f766
Lian <Lian.Duan@alumni.ubc.ca>
2019-06-15 13:34:43 -0700

    updated to standardize image

--- search.php
+++ search.php
@@ -1,1 +1,23 @@
 <?php
+    include('config/db_connect.php');
+
+    $sql = "select heroName, heroStatus, heroRank_ID, Hero_ID from Hero";
+    $sqlResult = mysqli_query($db, $sql);
+    $arrayOfHeroes = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+    mysqli_free_result($sqlResult);
+    mysqli_close($db);
+
+?>
+
+<html>
+<?php include('header.php'); ?>
+
+<div class="container">
+    <div class="row">
+
+
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
+</html>
