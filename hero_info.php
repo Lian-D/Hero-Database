3ee8c06e6febb8373a22618016466740967090d5
0x73006b <khan.k.shabab@gmail.com>
2019-06-15 08:58:11 -0700

    hero_info.php reads correctly, updated header

--- hero_info.php
+++ hero_info.php
@@ -1,1 +1,10 @@
 <?php
+
+$sql = "select heroName, heroStatus, heroRank_ID, Hero_ID from Hero where Hero_ID = $hero_id";
+$sqlResult = mysqli_query($db, $sql);
+$arrayOfHeroes = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+mysqli_free_result($sqlResult);
+mysqli_close($db);
+
+print_r($arrayOfHeroes);
+?>
