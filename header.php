8dcfd4f1f9cd8cf4d1f73525d28ac5bc737df4ee
0x73006b <khan.k.shabab@gmail.com>
2019-06-16 00:40:02 -0700

    fix simple url header

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
             <a href="index.php" class="brand-logo center">Hero Database</a>
             <ul class="right">
                 <li><a href="index.php">Home</a></li>
-                <li><a href="simplequeries.php.php">Simple Search</a></li>
+                <li><a href="simplequeries.php">Simple Search</a></li>
                 <li><a href="search.php">Advanced Search</a></li>
             </ul>
         </div>
--- simplequeries.php
+++ simplequeries.php
@@ -6,7 +6,11 @@ include('config/db_connect.php');
 <html lang="en">
 <?php include ('header.php'); ?>
 
-
+<div class="container">
+    <div class="card large">
+        Selection.
+    </div>
+</div>
 
 <?php include ('footer.php'); ?>
 </html>
