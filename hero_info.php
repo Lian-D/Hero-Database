80cbff39b71395ad69976291c8bd8ef8bbd7f6b5
0x73006b <khan.k.shabab@gmail.com>
2019-06-15 18:56:04 -0700

    salary, medals sql done -- gonna refactor this out eventually

--- hero_info.php
+++ hero_info.php
@@ -15,21 +15,45 @@ $sql = "select * from Hero where Hero_ID = $hero_id";
 $sqlResult = mysqli_query($db, $sql);
 $hero = mysqli_fetch_array($sqlResult, MYSQLI_ASSOC);
 
-// detailed info aka powerStats
-$sql = "select dex, durability, luck, strength, intelligence from PowerStats WHERE Hero_ID = 0";
-$sqlResult = mysqli_query($db, $sql);
-$powerStats = mysqli_fetch_array($sqlResult, MYSQLI_ASSOC);
 
 // to get age
 $sql = "select age from HeroDobAge where DOB in ( select DOB from Hero where Hero_ID = $hero_id)";
 $sqlResult = mysqli_query($db, $sql);
 $age = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
-$age = $age[0]['age'];
+
+// to get salary
+$sql ="select salary from HeroRank where Hero_ID = $hero_id";
+$sqlResult = mysqli_query($db, $sql);
+$salary = mysqli_fetch_array($sqlResult, MYSQLI_ASSOC);
+$salary = $salary['salary'];
+
+// setup vars for main info
+$name       = $hero['heroName'];
+$dob        = $hero['DOB'];
+$age        = $age[0]['age'];
+$height     = $hero['height'] . ' cm';
+$weight     = $hero['weight'] . ' lbs';
+$loc        = $hero['location'];
+$affil      = $hero['affiliation'];
+$heroStatus = $hero['heroStatus'];
+$heroRank   = $hero['heroRank_ID'];
+
+// detailed info aka powerStats
+$sql = "select dex, durability, luck, strength, intelligence from PowerStats WHERE Hero_ID = 0";
+$sqlResult = mysqli_query($db, $sql);
+$powerStats = mysqli_fetch_array($sqlResult, MYSQLI_ASSOC);
+
 
 // for Abilities
 $sql = "select abilityName from Ability where ability_ID IN (select ability_ID from CanDo where Hero_ID = $hero_id)";
 $sqlResult = mysqli_query($db, $sql);
 $abilityArray = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+
+// for Medals
+$sql = "select * from MedalHonors left join HasEarnedMedal on HasEarnedMedal.medal_ID = MedalHonors.medal_id
+        where (HasEarnedMedal.Hero_ID = $hero_id)";
+$sqlResult = mysqli_query($db, $sql);
+$medalsArray = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
 
 mysqli_free_result($sqlResult);
 mysqli_close($db);
@@ -40,7 +64,7 @@ mysqli_close($db);
 
 <div class="container">
     <div class="card large">
-        <span class="card-title"><h1><?php echo $hero['heroName'] ?></h1></span>
+        <span class="card-title"><h1><?php echo $name ?></h1></span>
         <div class="card-content">
             <div class="right">
                 <?php $img = 'media/' . $hero['Hero_ID'] . '.png';
@@ -49,15 +73,16 @@ mysqli_close($db);
                 <img src="<?php echo $img ?>">
             </div>
             <ul class="collection">
-                <li class="collection-item">Date of Birth: <?php echo $hero['DOB'] ?></li>
+<!--                <li class="collection-item">Name: --><?php //echo $name ?><!--</li>-->
                 <li class="collection-item">Age: <?php echo $age ?></li>
-                <li class="collection-item">Height: <?php echo $hero['height'] . ' cm' ?></li>
-                <li class="collection-item">Weight: <?php echo $hero['weight'] . ' lbs' ?></li>
-                <li class="collection-item">Location: <?php echo $hero['location'] ?></li>
-                <li class="collection-item">Affiliation: <?php echo $hero['affiliation'] ?></li>
-                <li class="collection-item">Status (Alive/Injured): <?php echo $hero['heroStatus'] ?></li>
-                <li class="collection-item">Rank: <?php echo $hero['heroRank_ID'] ?></li>
-                <li class="collection-item">Salary: <?php echo $hero['heroRank_ID'] ?></li>
+                <li class="collection-item">Date of Birth: <?php echo $dob ?></li>
+                <li class="collection-item">Height: <?php echo $height ?></li>
+                <li class="collection-item">Weight: <?php echo $weight ?></li>
+                <li class="collection-item">Location: <?php echo $loc ?></li>
+                <li class="collection-item">Affiliation: <?php echo $affil ?></li>
+                <li class="collection-item">Status (Alive/Injured): <?php echo $heroStatus ?></li>
+                <li class="collection-item">Rank: <?php echo $heroRank ?></li>
+                <li class="collection-item">Salary: ¥ <?php echo $salary ?></li>
             </ul>
         </div>
     </div>
@@ -66,9 +91,9 @@ mysqli_close($db);
         <span class="card-title"><h1>Detailed Info</h1></span>
         <div class="card-content">
             <ul class="collection">
-            <?php foreach ($powerStats as $key => $value): ?>
-                <li class="collection-item"><?php echo ucfirst(strtolower($key)). ": " . $value?></li>
-            <?php endforeach; ?>
+                <?php foreach ($powerStats as $key => $value): ?>
+                <li class="collection-item"><?php echo ucfirst(strtolower($key)). ": " . $value ?></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
@@ -78,7 +103,7 @@ mysqli_close($db);
         <div class="card-content">
             <ul class="collection">
                 <?php foreach ($abilityArray as $ability): ?>
-                    <li class="collection-item"> <?php echo($ability["abilityName"]); ?> </li>
+                <li class="collection-item"> <?php echo ($ability["abilityName"]); ?> </li>
                 <?php endforeach; ?>
             </ul>
         </div>
@@ -96,12 +121,16 @@ mysqli_close($db);
     <div class="card large">
         <span class="card-title"><h1>Medals</h1></span>
         <div class="card-content">
-            <?php
-            //foreach loop
-            ?>
+            <ul class="collection">
+            <?php foreach($medalsArray as $medal): ?>
+            <li class="collection-item"><strong><?php echo $medal['title']?></strong>
+                <br/>
+                Obtained on: <?php echo $medal['medalDate']?></li>
+            <?php endforeach ?>
+            </ul>
         </div>
     </div>
 </div>
 
-<?php include('footer.php'); ?>
+<?php include ('footer.php'); ?>
 </html>
