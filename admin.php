75c16c661a4a1cb289ec1168a8e1e0e520733cbe
c7s1b <khan.k.shabab@gmail.com>
2019-06-19 10:16:59 -0700

    half finished delete

--- admin.php
+++ admin.php
@@ -1,6 +1,14 @@
 <?php
 include('config/db_connect.php');
 
+
+// get usernames
+$sql = "select username from users";
+$result = mysqli_query($db, $sql);
+$usernameArray = mysqli_fetch_all($result);
+
+echo print_r($usernameArray);
+
 $admin = true;
 $showInterface = 'display: none !important;';
 if ($admin == true) {
@@ -20,23 +28,29 @@ mysqli_close($db);
     <div class="card">
         <h4 class="center" style="padding-top: 3vh">Admin Interface.</h4>
         <div class="row">
-            <div class="col s6 center">
-                <h5 class="center" style="">Update Tables.</h5>
-                Some stuff here. <p/>
-            </div>
-            <div class="col s6 center">
-                <h5 class="center" style="">Insert to Tables.</h5>
-                Some stuff here. <p/>
-            </div>
-        </div>
-        <div class="row">
             <div class="col s12 center">
-                <h5 class="center" style="">Delete Tables/Tuples.</h5>
-                Some stuff here. <p/>
+                <h5 class="center" style="">Delete user permissions.</h5>
+                <form>
+                <select class = "select" name="heroUserDropDown">
+                    <?php if (!empty($usernameArray)) { foreach ($usernameArray as $arrayResult): ?>
+                        <li class="select-item">
+                            <?php { echo "<option value='" . $arrayResult[0]."'>" .$arrayResult[0] ."</option>";} ?>
+                        </li>
+                    <?php endforeach; } ?>
+                </select>
+                </form>
+                <p/>
             </div>
         </div>
     </div>
 </div>
+
+
+<head>
+    <style>
+        .select {display: block;}
+    </style>
+</head>
 
 <?php include('footer.php'); ?>
 </html>
