c093a9e7a7d5a910668ff710f2d23bb010132798
sbhatti <shahbanobhatti@yahoo.com>
2019-06-19 01:46:23 -0700

    Added nested query.

--- aggregation.php
+++ aggregation.php
@@ -1,16 +1,35 @@
 <?php
 
-function doCount($db)
+function doCount($heroName,$db)
 {
 
-    $sql = "SELECT Hero.heroName AS 'Hero Name', COUNT(*) AS 'No. of Medals' 
-    FROM HasEarnedMedal , Hero 
-    WHERE Hero.Hero_ID = HasEarnedMedal.Hero_ID 
-    GROUP BY HasEarnedMedal.Hero_ID ";
+    $sql = "SELECT Hero.heroName AS 'Hero Name', COUNT(*) AS 'No. of Medals'
+            FROM Hero, HasEarnedMedal
+            WHERE Hero.Hero_ID = HasEarnedMedal.Hero_ID AND Hero.heroName = '$heroName'
+            GROUP BY HasEarnedMedal.Hero_ID";
+    //echo "SQL ". $sql;
     $sqlResult = mysqli_query($db, $sql);
     $abilityResult = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+    //echo print_r($abilityResult);
     mysqli_free_result($sqlResult);
     return $abilityResult;
 }
+
+function doNested($Name,$db) {
+    $sql = "SELECT MAX(salary), Hero.affiliation AS Association
+            FROM HeroRank, Hero
+            WHERE HeroRank.Hero_ID = Hero.Hero_ID AND Hero.affiliation ='$Name' 
+              AND Hero.Hero_ID IN (SELECT Winner
+                             FROM FightResult)
+            GROUP BY Hero.affiliation";
+    //echo "SQL ". $sql;
+    $sqlResult = mysqli_query($db, $sql);
+    $abilityResult = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+    //echo print_r($abilityResult);
+    mysqli_free_result($sqlResult);
+    return $abilityResult;
+}
+
+
 
 ?>
