fc9ff378aed2342513ac423b42b0e917ae570fae
Lian <Lian.Duan@alumni.ubc.ca>
2019-06-19 15:22:37 -0700

    division updated

---
+++ division.php
@@ -0,0 +1,26 @@
+<?php
+    function doDivision($db)
+    {
+//        echo $dex."\n ";
+//        echo $dur."\n ";
+//        echo $luk."\n ";
+//        echo $str."\n ";
+//        echo $int."\n ";
+//        echo $date."\n ";
+
+        $sql = "SELECT h.Hero_ID, h.heroName 
+            FROM Hero h
+            Where not EXISTS(select *
+                 from Ability a
+                 where not EXISTS(
+                     SELECT * 
+                     from CanDo c
+                     where c.ability_ID = a.ability_ID
+                     and c.Hero_ID = h.Hero_ID))";
+
+        $sqlResult = mysqli_query($db, $sql);
+//        echo print_r($sqlResult);
+        $result = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+        mysqli_free_result($sqlResult);
+        return $result;
+    }
