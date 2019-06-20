18610d4f2d3726fd19105b95557d37064bfe96b8
Lian <Lian.Duan@alumni.ubc.ca>
2019-06-19 23:08:58 -0700

    selection

---
+++ selection.php
@@ -0,0 +1,12 @@
+<?php
+    function doSelection($field1, $field2, $field3, $db)
+    {
+        $sql = "SELECT". $field1.
+            "FROM".$field2.
+            "Where".$field3;
+
+        $sqlResult = mysqli_query($db, $sql);
+        $result = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+        mysqli_free_result($sqlResult);
+        return $result;
+    }
